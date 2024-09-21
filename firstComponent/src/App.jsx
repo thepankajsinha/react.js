9ae6be5bb9bb40730a5fab2f2210b63db99f04c5
Pankaj kumar <askpankajsinha@gmail.com>
2024-09-21 17:59:53 +0530

    update

---
+++ firstComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./Components/HelloWorld";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+    </div>
+  )
+}
+
+export default App
---
+++ firstComponent/src/Components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HelloWorld.css"
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <button>Click me!</button>
+    </div>
+  )
+}
+
+export default HelloWorld
