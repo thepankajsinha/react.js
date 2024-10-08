2907586494a9dc4e5a09c3fb97bf1b5380beb62c
Pankaj Sinha <136917326+thepankajsinha@users.noreply.github.com>
2024-10-08 14:11:31 +0530

    Add files via upload

---
+++ EventHandling/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+function App() {
+
+	const [inp, setINP] = useState("Button is not clicked!");
+	const handleClick = () => {
+		setINP("Button is clicked");
+	};
+  
+	return (
+		<div className="App">
+
+      {<h2>{inp}</h2>}
+      <button onClick={handleClick}>Click me!</button> 
+
+		</div>
+	);
+}
+export default App;
