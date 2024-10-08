73f73873d57f9923f54f066d4301f28db9676663
Pankaj Sinha <136917326+thepankajsinha@users.noreply.github.com>
2024-10-08 12:38:50 +0530

    Add files via upload

---
+++ Component/src/App.jsx
@@ -0,0 +1,12 @@
+//Importing a component
+import HelloWorldComponent from "./HelloWorldComponent"
+
+function App() {
+  return (
+    <>
+      <HelloWorldComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ Component/src/HelloWorldComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//Creating a component
+function HelloWorldComponent() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+//Exporting the component
+export default HelloWorldComponent
